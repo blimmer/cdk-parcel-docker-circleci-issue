3bbd95dbc5c2ef31fdcfb49ebdf1bca87a2dd6e4
Ben Limmer <ben@benlimmer.com>
2020-05-11 11:09:34 -0600

    Show issue (#1)

--- lib/cdk-parcel-docker-circleci-issue-stack.ts
+++ lib/cdk-parcel-docker-circleci-issue-stack.ts
@@ -1,9 +1,13 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as nodelambda from "@aws-cdk/aws-lambda-nodejs";
+import * as path from "path";
 
 export class CdkParcelDockerCircleciIssueStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new nodelambda.NodejsFunction(this, "MyNodeLambda", {
+      entry: path.join(__dirname, "index.ts"),
+    });
   }
 }
